ca125aa9f8ff15749f3a0efbdbf3962d89487e35
Philipp Wüthrich <philipp.wuethrich@swisscom.com>
2018-12-25 17:43:39 +0100

    add team endpoint

---
+++ HandballResults/HandballResults/Controllers/TeamsController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using HandballResults.Models;
+using HandballResults.Services;
+
+namespace HandballResults.Controllers
+{
+    [RoutePrefix("teams")]
+    public class TeamsController : Controller
+    {
+
+        private static readonly IResultService ResultService = new CachedShvResultService();
+
+        // GET: teams/{teamId}
+        [Route("{teamId}")]
+        public async Task<ActionResult> Index(int teamId)
+        {
+            IEnumerable<Game> schedule = new List<Game>();
+            IEnumerable<Game> results = new List<Game>();
+            Group group = null;
+            ServiceException error = null;
+
+            try
+            {
+                schedule = await ResultService.GetScheduleAsync(teamId);
+                results = await ResultService.GetResultsAsync(teamId);
+                group = await ResultService.GetGroupForTeam(teamId);
+            }
+            catch (ServiceException e)
+            {
+                error = e;
+            }
+
+            return View(new TeamsViewModel(schedule, results, group, error));
+        }
+    }
+}
---
+++ HandballResults/HandballResults/Models/TeamsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using HandballResults.Services;
+
+namespace HandballResults.Models
+{
+    public class TeamsViewModel
+    {
+        public IEnumerable<Game> Schedule { get; set; }
+
+        public IEnumerable<Game> Results { get; set; }
+
+        public Group Group { get; }
+
+        public ServiceException Error { get; set; }
+
+        public TeamsViewModel(IEnumerable<Game> schedule, IEnumerable<Game> results, Group group, ServiceException error)
+        {
+            Schedule = schedule;
+            Results = results;
+            Group = group;
+            Error = error;
+        }
+    }
+}
