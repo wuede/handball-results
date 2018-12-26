327dced9ab8a1942507c37c9f242b3b86de279e0
Philipp Wüthrich <philipp.wuethrich@swisscom.com>
2018-12-26 11:07:19 +0100

    - change color scheme - add global error handler

--- HandballResults/HandballResults/Global.asax.cs
+++ HandballResults/HandballResults/Global.asax.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -16,6 +12,12 @@ namespace HandballResults
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error()
+        {
+            var exception = Server.GetLastError();
+            System.Diagnostics.Trace.TraceError("uncaught exception: {0}", exception);
         }
     }
 }
